940c5eb38fed27f1889e8eb89700cb3ec90b9bcf
vladgx <Vlad.gx@gmail.com>
2018-11-30 14:46:55 +0200

    updated

--- app/myContentPane.js
+++ app/myContentPane.js
@@ -1,7 +1,5 @@
-/**
- * Created by vladgx on 30.11.2018.
- */
-define(["../dojo/_base/declare","../dojox/layout/ContentPane"],
+
+define(["dojo/_base/declare","dojox/layout/ContentPane"],
         function(declare,ContentPane){
             return declare("myContentPane",ContentPane,{
                 title:"myTitle",
